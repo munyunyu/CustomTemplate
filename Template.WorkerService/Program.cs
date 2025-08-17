8243ac92e34eca99f51b9d6f5dc14be80c9d8804
munyunyu <percym@electronicline.com>
2025-08-17 18:54:56 +0200

    Added console app and worker service

---
+++ Template.WorkerService/Program.cs
@@ -0,0 +1,10 @@
+using Template.WorkerService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
