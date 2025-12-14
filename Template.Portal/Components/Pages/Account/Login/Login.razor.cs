55cd54f4157b8fcda6a5edabc9a87af5becd7ba1
percy.munyunyu <percy.munyunyu@live.com>
2025-12-14 13:25:39 +0200

    Added CascadingAuthenticationState and access control

--- Template.Portal/Components/Pages/Account/Login/Login.razor.cs
+++ Template.Portal/Components/Pages/Account/Login/Login.razor.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
 using System.Security.Claims;
+using System.Security.Principal;
+using Template.Library.Extensions;
 using Template.Portal.Components.Shared.Helpers;
 using Template.Portal.Services;
+using Template.Portal.Services.System;
 
 namespace Template.Portal.Components.Pages.Account.Login
 {
     public partial class Login : BasePage
     {
+        [Inject] public required AuthenticationStateProvider _authProvider { get; set; }
+        [Inject] public required IHttpContextAccessor _httpContextAccessor { get; set; }
         protected override async Task OnInitializedAsync()
         {
            
@@ -18,12 +25,28 @@ namespace Template.Portal.Components.Pages.Account.Login
                  Password = "tc#Prog219!"
             });
 
-            //var claims = user.GetUserClaims();
+            var claims = user.GetUserClaims();
 
-            //var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            //var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+
+            var principal = new ClaimsPrincipal(identity);
 
             //await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
 
+            if (_httpContextAccessor.HttpContext != null)
+            {
+                await _httpContextAccessor.HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    principal,
+                    new AuthenticationProperties
+                    {
+                        IsPersistent = true,
+                        ExpiresUtc = DateTimeOffset.UtcNow.AddDays(7)
+                    });
+            }
+
+            NavigationManager.NavigateTo("/", forceLoad: true);
         }
     }
 }
--- Template.Portal/Program.cs
+++ Template.Portal/Program.cs
@@ -1,11 +1,30 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Template.Portal.Components;
 using Template.Portal.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddRazorComponents().AddInteractiveServerComponents();
 builder.Services.AddCustomScopedServices(builder);
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/account/login";
+        options.LogoutPath = "/logout";
+        options.AccessDeniedPath = "/access-denied";
+        options.Cookie.HttpOnly = true;
+        options.SlidingExpiration = true;
+        options.ExpireTimeSpan = TimeSpan.FromHours(8);
+    });
+
+builder.Services.AddCascadingAuthenticationState();
+//builder.Services.AddScoped<AuthenticationStateManager>();
+builder.Services.AddHttpContextAccessor();
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,6 +37,8 @@ if (!app.Environment.IsDevelopment())
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseAntiforgery();
 
 app.MapStaticAssets();
