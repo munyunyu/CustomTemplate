2f49a0c822c2bfba1f9490582f93b6d998eef350
percy.munyunyu <percy.munyunyu@live.com>
2025-12-18 14:49:29 +0200

    Added view to be created on database creation

---
+++ Template.Database/Migrations/20251218124207_Added_Views01.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Template.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_Views01 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+            CREATE OR ALTER VIEW [dbo].[ViewApplicationUserRoles]
+            AS
+            SELECT TOP (1000) 
+                   [UserId][Id]
+                  ,[RoleId]
+            	  ,U.Email
+            	  ,R.[Name]
+            	  ,[CreatedDate]
+                  ,[CreatedById]
+                  ,[LastUpdatedDate]
+                  ,[LastUpdatedById]
+                  ,[IsDeleted]
+              FROM [TemplateDB].[dbo].[AspNetUserRoles]UR
+              LEFT JOIN [TemplateDB].[dbo].[AspNetUsers]U ON U.[Id] = UR.[UserId]
+              LEFT JOIN [TemplateDB].[dbo].[AspNetRoles]R ON R.[Id] = UR.[RoleId]
+            ");
+
+            migrationBuilder.Sql(@"
+            CREATE OR ALTER VIEW [dbo].[ViewApplicationUser]
+            AS
+            SELECT  
+                [Id],
+                [FirstName],
+                [LastName],
+                [CreatedDate],
+                [LastUpdatedDate],
+                [IsDeleted],
+                [UserName],
+                [NormalizedUserName],
+                [Email],
+                [NormalizedEmail],
+                [EmailConfirmed],
+                [PasswordHash],
+                [SecurityStamp],
+                [ConcurrencyStamp],
+                [PhoneNumber],
+                [PhoneNumberConfirmed],
+                [TwoFactorEnabled],
+                [LockoutEnd],
+                [LockoutEnabled],
+                [AccessFailedCount],
+                [CreatedById],
+                [LastUpdatedById],
+                [Description],
+                [Hash]
+            FROM [dbo].[AspNetUsers];
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DROP VIEW IF EXISTS [dbo].[ViewApplicationUserRoles];");
+            migrationBuilder.Sql(@"DROP VIEW IF EXISTS [dbo].[ViewApplicationUser];");
+        }
+    }
+}
